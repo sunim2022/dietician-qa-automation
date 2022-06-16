51539e16eca26efc1e1fc75f8d6cced8a91891d8
sunim2022 <suni.mothadikala@gmail.com>
2022-06-16 13:14:32 -0400

    Added testng

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,5 +1,7 @@
 package runner;
 
+import org.testng.annotations.DataProvider;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
@@ -12,5 +14,10 @@ import io.cucumber.testng.CucumberOptions;
 )
 public class TestRunner extends AbstractTestNGCucumberTests{
 	
-//
+	@Override
+    @DataProvider(parallel = false)
+    public Object[][] scenarios() {
+       return super.scenarios();
+    }
+
 }
